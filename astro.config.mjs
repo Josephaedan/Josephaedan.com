6ae54e53c5adf078645ee86ec0352ea912487f8c
Joseph Aedan Marcus <josephaedan@gmail.com>
2024-05-18 21:43:30 +0800

    add site property to config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
 import { defineConfig } from 'astro/config';
 
+const site = import.meta.env.DEPLOYED_URL || "https://www.josephaedan.com";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: site,
+});
