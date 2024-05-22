71cefd1312ca840b238c6c8248b86659682aa19f
Joseph Aedan Marcus <josephaedan@gmail.com>
2024-05-23 00:24:16 +0800

    update collections config

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,17 +1,31 @@
 // 1. Import utilities from `astro:content`
-import { defineCollection, z } from "astro:content";
+import {
+  EducationFrontmatter,
+  ExperienceFrontmatter,
+  TechnologyFrontmatter,
+} from "@/types/collections";
+import { defineCollection, reference, z } from "astro:content";
+
 // 2. Define your collection(s)
-const cardCollection = defineCollection({
+const educationCollection = defineCollection({
   type: "content",
-  schema: z.object({
-    Title: z.string(),
-    Subtitle: z.string(),
-    Date: z.string(),
-    Published: z.date(),
-  }),
+  schema: EducationFrontmatter,
 });
+
+const technologyCollection = defineCollection({
+  type: "data",
+  schema: TechnologyFrontmatter,
+});
+
+const experienceCollection = defineCollection({
+  type: "content",
+  schema: ExperienceFrontmatter,
+});
+
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
 export const collections = {
-  education: cardCollection,
+  education: educationCollection,
+  technologies: technologyCollection,
+  experience: experienceCollection,
 };
