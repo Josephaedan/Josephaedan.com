eee3b19bfe0cef4db5c0de1a8623e581f572dc85
Joseph Aedan Marcus <josephaedan@gmail.com>
2024-05-25 11:44:56 +0800

    add web analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from 'astro/config';
+import vercel from "@astrojs/vercel/serverless";
 
 const site = import.meta.env.DEPLOYED_URL || "https://www.josephaedan.com";
 
 // https://astro.build/config
 export default defineConfig({
   site: site,
+  output: "server",
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+  }),
 });
