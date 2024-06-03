84fdb95f4657fcf7d435a3d1e2f6eb448780c2f6
Joseph Aedan Marcus <josephaedan@gmail.com>
2024-06-03 22:34:24 +0800

    add sitemap.xml

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import vercel from "@astrojs/vercel/serverless";
+import sitemap from "@astrojs/sitemap";
 
 const site = import.meta.env.DEPLOYED_URL || "https://www.josephaedan.com";
 
 // https://astro.build/config
 export default defineConfig({
   site: site,
+  integrations: [sitemap()],
   output: "server",
   adapter: vercel({
     webAnalytics: { enabled: true },
